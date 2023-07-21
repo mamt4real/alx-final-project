be8f05befc2e4f8b5ddf84eb260802dcff86d07b
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2023-07-21 18:25:01 +0100

    Setting up phpto

---
+++ src/models/Photo.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose')
+
+const photoSchema = Schema(
+  {
+    name: String,
+    description: String,
+    url: {
+      type: String,
+      required: [true, 'url is missing'],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'owener id is required'],
+    },
+    likes: {
+      type: [Schema.Types.ObjectId],
+      ref: 'User',
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+)
+
+photoSchema.pre(/^find/, function (next) {
+  this.populate('owner', 'image username')
+})
+
+photoSchema.pre('findOne', function (next) {
+  this.populate('likes', 'image username')
+})
+
+const Photo = model('Photo', photoSchema)
+module.exports = Photo
---
+++ src/v1/controllers/photoController.js
@@ -0,0 +1,25 @@
+const Photo = require('../../models/Photo')
+const catchAsync = require('../../utils/catchAsync')
+const GeneralController = require('../../utils/handlerFactory')
+
+const downloadPhoto = catchAsync(async (req, res, next) => {
+  res.status(200).send('Not Implemented')
+})
+
+const uploadPhoto = catchAsync(async (req, res, next) => {
+  res.status(200).send('Not Implemented')
+})
+
+const likeDislikePhoto = catchAsync(async (req, res, next) => {
+  res.status(200).send('Not Implemented')
+})
+
+const getAllPhotos = GeneralController.getAll(Photo)
+const getOnePhoto = GeneralController.getOne(Photo)
+
+module.exports = {
+  getAllPhotos,
+  getOnePhoto,
+  downloadPhoto,
+  uploadPhoto,
+}
