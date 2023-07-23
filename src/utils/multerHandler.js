ba340c66e800eab028cd4165858a38bc13f88652
MAHADI ABUHURAIRA <mamt4real@gmail.com>
2023-07-23 20:07:02 +0100

    Add Single photo

--- src/utils/multerHandler.js
+++ src/utils/multerHandler.js
@@ -36,17 +36,6 @@ const imageFilter = function (req, file, cb) {
   cb(null, true)
 }
 
-const csvFilter = function (req, file, cb) {
-  file.mimetype === 'text/csv'
-    ? cb(null, true)
-    : cb(new BadRequest('You can only upload a single csv file'), false)
-}
-
-const uploadSingleCsvFile = multer({
-  storage: memStorage,
-  fileFilter: csvFilter,
-}).single('file')
-
 /**
  * Middleware to resize a profile image into square
  */
@@ -58,7 +47,7 @@ const resizeUserPhoto = catchAsync(async (req, res, next) => {
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`)
+    .toFile(`public/images/users/${req.file.filename}`)
 
   req.body.image = `${req.protocol}://${req.get('host')}/images/${
     req.file.filename
@@ -66,35 +55,34 @@ const resizeUserPhoto = catchAsync(async (req, res, next) => {
   next()
 })
 
-const resizePhoto = catchAsync(async (req, res, next) => {})
+/**
+ * Middleware to resize an uploaded photo into 4:3 aspect ratio
+ */
+const resizeSinglePhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next()
+  const filename = `photo-${req.user?._id}-${req.file.originalname}.jpg`
+  await sharp(inputImagePath)
+    .resize({
+      width: 800,
+      height: 600,
+      fit: sharp.fit.inside,
+      position: sharp.strategy.entropy,
+    })
+    .jpeg({ quality: 85 })
+    .toFile(`public/images/photos/${filename}`)
+  req.body.url = `${req.protocol}://${req.get('host')}/images/${filename}`
+  next()
+})
 
-const inputImagePath = 'path/to/input/image.jpg'
-const outputImagePath = 'path/to/output/resized-image.jpg'
-
-// Desired output image properties
-const outputWidth = 800 // The width you want for the resized image
-const outputHeight = 600 // The height you want for the resized image
-const aspectRatio = '4:3' // The desired aspect ratio, 4:3 in this example
-const quality = 80 // Quality factor between 1 and 100
-
-sharp(inputImagePath)
-  .resize({
-    width: outputWidth,
-    height: outputHeight,
-    fit: sharp.fit.inside,
-    position: sharp.strategy.entropy,
-  })
-  .jpeg({ quality: quality })
-  .toFile(outputImagePath, (err, info) => {
-    if (err) {
-      console.error('Error resizing image:', err)
-    } else {
-      console.log('Image resized and saved successfully.')
-    }
-  })
-
-// const uploadSingleImage
+/**
+ * Middleware to upload a single photo
+ */
+const uploadSinglePhoto = multer({
+  storage: memStorage,
+  fileFilter: imageFilter,
+}).single('image')
 
 module.exports = {
-  uploadSingleCsvFile,
+  uploadSinglePhoto,
+  resizeSinglePhoto,
 }
